7aec619183159adeec19c11a7874fd0ef5aa8f94
Dominik Seemayr <seemayr@me.com>
2018-04-24 13:09:27 +0200

    test

--- app/src/main/java/com/hagenberg/needy/MainActivity.java
+++ app/src/main/java/com/hagenberg/needy/MainActivity.java
@@ -3,6 +3,8 @@ package com.hagenberg.needy;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+//bissle testen
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
