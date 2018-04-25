0cec0964a3be978f6cd4232bda7f7e8cd14bc603
David Schindler <david.schindler97@gmail.com>
2018-04-25 17:05:59 +0200

    appname geändert

--- app/src/main/java/com/hagenberg/needy/CreateRecipeActivity.java
+++ app/src/main/java/com/hagenberg/needy/CreateRecipeActivity.java
@@ -1,13 +1,24 @@
 package com.hagenberg.needy;
 
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class CreateRecipeActivity extends AppCompatActivity {
+public class CreateRecipeActivity extends AppCompatActivity implements CreateRecipeFragment.OnFragmentInteractionListener{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_recipe);
+    }
+
+    @Override
+    public void onFragmentInteraction(Uri uri) {
+
+    }
+
+    @Override
+    public void onPointerCaptureChanged(boolean hasCapture) {
+
     }
 }
