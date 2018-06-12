678872789d95b1bbbab2453db2d16d8129003f38
David Schindler <david.schindler97@gmail.com>
2018-06-12 16:23:58 +0200

    Zurueck zum Homescreen über ActionbarPfeil

--- app/src/main/java/com/hagenberg/needy/Activities/CreateRecipeActivity.java
+++ app/src/main/java/com/hagenberg/needy/Activities/CreateRecipeActivity.java
@@ -13,6 +13,7 @@ public class CreateRecipeActivity extends AppCompatActivity implements CreateRec
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_recipe);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
     }
 
     @Override
